077604d809bf93cb67bfd581edf2d8dd17e0145a
naveenaguthi <81163223+naveenaguthi@users.noreply.github.com>
2021-12-19 16:11:05 +0530

    Create web.js

---
+++ web.js
@@ -0,0 +1,45 @@
+let fs=require("fs");
+let xslsx=require("xslx");
+style{
+  background color="Blue",
+  text-color="Black"
+}
+let buffer=fs.readFileSync("
+let data=require("
+console.log(data);
+data.push({
+  "name":"BMS college of Engineering",
+   "place":"Bangalore",
+    "Courses":"Engineering,Architecture.Management,B.tech,Phd"
+     "EStablished":1985,
+      "Exams":"CET,COMEDK,GOI,CSAB"
+}
+name":"Atria institute of Technology",
+   "place":"Bangalore",
+    "Course"s":"Engineering,Architecture,Management,B.tech,Phd"
+     "EStablished":2000,
+      "Exams":"Karantaka PGCET,Commdek,kcet"
+          }
+"name":"RV college of Engineering",
+   "place":"Bangalore",
+    "Courses":"Engineering,Architecture.Management,B.tech,Phd"
+     "EStablished":1963,
+      "Exams":" PGCET,GATE,kcet"
+}
+"name":"Ramaiah institute of Technology",
+   "place":"Bangalore",
+    "Courses":"Engineering,Architecture,Management,B.tech,Phd"
+     "EStablished":1962,
+      "Exams":"MSRIT"
+}
+"name":"Atria institute of Technology",
+   "place":"Bangalore",
+    "Course"s":"Engineering,Architecture,Management,B.tech,Phd"
+     "EStablished":1999,
+      "Exams":"Jee mains,CET"
+});
+let newWB=xlsx.utils.book_new();
+let newWs=xlsx.utils.json_to_sheet(json);
+xlsx.utils.book_append_sheet(newWB,newWs,webdev);
+xlsx.writeFile(newWB,"C:\Users\GUTHI NAVEENA\OneDrive\Desktop\webdev.xlsx")
+          
